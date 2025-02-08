5ccfcecb35f6c587e4bf4a1e6d2c5e207248bead
Sebastian Gonzalo Alvarez Concepcion <sebastianalvarezconcepcion@gmail.com>
2025-02-08 15:06:25 -0400

    Add: Tarea2CreeUnProgramaSencillo

--- Introduccion a C#/Tarea2CreeUnProgramaSencillo/Tarea2CreeUnProgramaSencillo/Program.cs
+++ Introduccion a C#/Tarea2CreeUnProgramaSencillo/Tarea2CreeUnProgramaSencillo/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+ * Sebastian Alvarez 
+ * Matricula: 2024-1670
+ * Fecha: 8-2-2025
+ */
+
+/* Usando los conocimientos adquiridos, 
+ * usted debe desarrollar un programa que le 
+ * solicite al usuario un numero y el programa indique si es par o impar
+ */
+
+Console.Write("Ingrese un numero entero: ");
+try
+{
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+    if (numero % 2 == 0) Console.WriteLine("El numero es par");
+    else Console.WriteLine("El numero es impar");
+
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("¿No ves que dice numero entero?");
+    Console.WriteLine("Vuelve a ejecutar el programa");
+}catch(OverflowException ex)
+{
+    Console.WriteLine("El limite permitido para este numero es 2,147,483,647");
+}
+
+
